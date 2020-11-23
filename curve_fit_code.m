574fc367cab9373c0f4959f2158319c314d4e6c8
Moeeze Hassan <fammas.maz@gmail.com>
2020-11-24 00:59:49 +0500

    Image input only

--- curve_fit_code.m
+++ curve_fit_code.m
@@ -1,83 +1,64 @@
-obj = VideoReader('fin.mov'); % Reads the Video
-vid = read(obj);
-frames = obj.NumberOfFrames; % Exports the total number of frames to a variable
-warning('off'); % Removes all warnings
-t = 1;  % Change this value to skips frames
+RGB = imread('im.png');
+I = rgb2gray(RGB);
+I = imcomplement(I);
+imshow(I)
+BW = im2bw(I, 0.4);
 
-x = 1:t:frames; % Array of all frames
-le = length(x);
 
-currDate = strrep(datestr(datetime), ' ', '_');
-baseFolder = '/Users/fammas.maz/Downloads/matlab_image/images/';
+imtool(BW);
+[row, col] = find(BW==0);
+st = sqrt((row(1)-row(2))^2 + (col(1)-col(2))^2 );
+rowbc = zeros(size(row,1),1);
+colbc = zeros(size(row,1),1);
 
-destinationFolder = strcat(baseFolder, currDate, '/');
 
-mkdir(destinationFolder);
+for i = 2:(size(row,1)-2)
+    lun1 = sqrt((row(i)-row(i+1))^2 + (col(i+1)-col(i))^2 );
+    lun2 = lun1 = sqrt((row(i)-row(i+2))^2 + (col(i+2)-col(i))^2 );
+    if (lun1 < 1*st)
+        rowbc(i) = 1;
+        colbc(i) = 1;
+        i = i+1;
+    end
+end
+row = row(~rowbc);
+col = col(~colbc);
 
-j = 1;
 
-for x = 1:t:frames
 
-    imwrite(vid(:,:,:,x),strcat(destinationFolder,num2str(j),'.png')); % Writes all frames to a the current dir
-    j = j+1;
+row = -1*row;
+y = row;
+x = col;
 
+i = 1;
+errorfinal = 100;
+
+while ((errorfinal > 30) && (i <= 5))
+    
+    
+    [p,S,mu] = polyfit(x,y,i);
+    [v,delta] = polyval(p, x, S, mu);
+
+    i = i+1;
+    
+
+    err = delta.^2;
+    err_mean = mean(err);
+    errorfinal = sqrt(err_mean);
 end
 
-dinfo = dir(strcat(destinationFolder,'*.png')); % Selects all the png files extracted earlier
-filenames = {dinfo.name};
-sorted = natsortfiles(filenames); % Puts them sorted into a cell
+p = polyfit(x,y,i-1);
+v = polyval(p, x);
 
-s = numel(sorted);
-d = destinationFolder;
+plot(x,y,'o',x,v);
 
-% Creating different cells for variables
+fprintf("The Degree of the Equation Fitted is %d\n", i-1)
+fprintf("The Confidence of the fit is is %0.3f%\n", errorfinal)
 
-c = cell(1,s);
-p = cell(1,s);
-v = cell(1,s);
-mu = cell(1,s);
-S = cell(1,s);
-delta = cell(1,s);
-y = cell(1,s);
-x = cell(1,s);
-errorfinal =cell(1,s);
-
-for k=1:s
-  % Image Processing
-       F = fullfile(d, sorted(1, k));
-       F = cell2mat(F);
-       c{k} = imread(F);
-       I = rgb2gray(c{k});
-       BW = im2bw(I, 0.2);
-% Collecting Data Points
-       [row, col] = find(BW==0);
-       row = -1*row;
-       y{k} = row;
-       x{k} = col;
-
-       i = 1;
-       errorfinal{k} = 100;
-
-% Curve fitting and iterating over a single frame 
-       while ((errorfinal{k} > 5) && (i <= 10))
-           [p{k},S{k},mu{k}] = polyfit(x{k},y{k},i); % Outputs the values of p, S, mu
-           [v{k},delta{k}] = polyval(p{k}, x{k}, S{k}, mu{k}); % Outputs the values of v and delta which is abs error
-           i = i+1;
-           err = delta{k}.^2;
-           err_mean = mean(err);
-           errorfinal{k} = sqrt(err_mean); % Finds Confidence of the fit
-       end
-       p{k} = polyfit(x{k},y{k},i-1);
-       v{k} = polyval(p{k}, x{k});
-
-       fprintf("\n\nFrame %d Info: \n\n", k)
-       fprintf("The Degree of the Equation Fitted is %d\n", i-1)
-       fprintf("The Confidence of the fit in Frame %0.3f\n", errorfinal{k})
-
-       u =length(p{k});
+grid on
 
 
-       fprintf(['Equation of the curve is:\ny = ' repmat('%0.16fx + ',1,numel(p{k}))] , p{k} )
+fprintf(['Equation of the curve is:\ny = ' repmat('%0.8fx + ',1,numel(p))] , p )
 
 
-end
+
